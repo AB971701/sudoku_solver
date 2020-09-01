e55ced247818874443d2053649db1fc8b2095fb9
AB971701 <bakocovaanezka@seznam.cz>
2020-09-01 20:17:54 +0200

    Update main.py
    added solving algorithm and animation

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import tkinter as tk
 GRID_OFFSET = 10
 MIN_CELL_SIZE = 50
 
-FONT_STYLE = 'Consolas'
+FONT_STYLE = 'Courier'
 FONT_SIZE = 20
 
 task = [
@@ -55,18 +55,82 @@ def create_grid():
 
 
 def populate_grid():
+    """
+    populates Sudoku grid with given numbers
+    """
     for row in range(9):
         for col in range(9):
-            subsquare_index = subsquare_numbers[str(row // 3) + ',' + str(col // 3)]
-            row_index = row % 3
-            col_index = col % 3
-            lbl_num = labels[subsquare_index][row_index][col_index]
-
             num = task[row][col]
             if num != 0:
+                subsquare_index = subsquare_numbers[str(row // 3) + ',' + str(col // 3)]
+                row_index = row % 3
+                col_index = col % 3
+                lbl_num = labels[subsquare_index][row_index][col_index]
                 str_num = str(num)
                 lbl_num['text'] = str_num
                 lbl_num.config(fg='white')
+
+
+def find_next_free_cell(task, row, col):
+    for r in range(row, 9):
+        for c in range(col, 9):
+            if task[r][c] == 0:
+                return r, c
+    return None, None
+
+
+def num_valid_in_row(task, row, num):
+    for col in range(9):
+        if task[row][col] == num:
+            return False
+    return True
+
+
+def num_valid_in_column(task, col, num):
+    for row in range(9):
+        if task[row][col] == num:
+            return False
+    return True
+
+
+def num_valid_in_square(task, row, col, num):
+    init_row = row - row % 3
+    init_col = col - col % 3
+    for r in range(init_row, init_row + 3):
+        for c in range(init_col, init_col + 3):
+            if task[r][c] == num:
+                return False
+    return True
+
+
+def num_is_valid(task, row, col, num):
+    return num_valid_in_row(task, row, num) and num_valid_in_column(task, col, num) and num_valid_in_square(task, row, col, num)
+
+
+def change_num_in_grid(row, col, str_num):
+    subsquare_index = subsquare_numbers[str(row // 3) + ',' + str(col // 3)]
+    row_index = row % 3
+    col_index = col % 3
+    lbl_num = labels[subsquare_index][row_index][col_index]
+    lbl_num['text'] = str_num
+    lbl_num.update()
+
+
+def solve_sudoku():
+    row = col = 0
+    row, col = find_next_free_cell(task, row, col)
+    if col is None:
+        return True
+    for num in range(1, 10):
+        change_num_in_grid(row, col, str(num))
+        if num_is_valid(task, row, col, num):
+            task[row][col] = num
+            if solve_sudoku():
+                return True
+            task[row][col] = 0
+            change_num_in_grid(row, col, '')
+    change_num_in_grid(row, col, '')
+    return False
 
 
 window = tk.Tk()
@@ -76,5 +140,6 @@ frm_main.pack(padx=GRID_OFFSET, pady=GRID_OFFSET, expand=True)
 
 labels = create_grid()
 populate_grid()
+solve_sudoku()
 
 window.mainloop()
